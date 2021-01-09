5d3641958db9187ee339b480d511b7ac8083d802
Protik111 <rafiurprotik111.gmail.com>
2021-01-09 12:52:06 +0600

    facebook sign in added

--- src/App.js
+++ src/App.js
@@ -125,11 +125,43 @@ function App() {
       setUser(newUserInfo);
     }
   }
+
+  const fbProvider = new firebase.auth.FacebookAuthProvider();
+  
+  const handleFbSignIn = () => {
+    firebase.auth().signInWithPopup(fbProvider)
+      .then((res) => {
+        // {firebase.auth.OAuthCredential}
+        // var credential = result.credential;
+
+        // The signed-in user info.
+        const user = res.user;
+        console.log('fb user after sign  in', user.displayName);
+
+        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        // var accessToken = credential.accessToken;
+
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+
+        // ...
+      });
+  }
   return (
     <div className="App">
       { user.isSignedIn ? <button onClick={handleSignOut}>Sign Out</button> :
         <button onClick={handleSignIn}>Sign In</button>
       }
+      <br/>
+      <button onClick={handleFbSignIn}>Sign In With Facebook</button>
       {
         user.isSignedIn && <div>
           <p>Welcome, {user.name}</p>
